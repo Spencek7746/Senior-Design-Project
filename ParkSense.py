b3ae34a509c722385a14bf55545558be5379f185
Spencer Karpati <spebkarp@ut.utm.edu>
2023-09-03 18:21:43 -0500

    Added parking lot jpeg and matplotlib logic

--- ParkSense.py
+++ ParkSense.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np
+import matplotlib.pylab as plt
 
-image = np.zeros((512, 512, 3), np.uint8)
+parking_lot_files = "parking_lot_empty.jpg"
+image = plt.imread(parking_lot_files)
 
-cv2.line(image, (0, 0), (511, 511), (0, 255, 0), 5)
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.imshow(image)
+ax.axis('off')
+plt.show()
 
-cv2.imshow('Image', image)
+#image = np.zeros((512, 512, 3), np.uint8)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.line(image, (0, 0), (511, 511), (0, 255, 0), 5)
+
+#cv2.imshow('Image', image)
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
