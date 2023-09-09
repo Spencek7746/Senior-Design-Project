f7c55897b5df65aedfdc7e9328d0bbba6e3176b1
Spencer Karpati <spebkarp@ut.utm.edu>
2023-09-09 12:46:04 -0500

    Added absract and updated .tex file

--- ParkSense.py
+++ ParkSense.py
@@ -2,11 +2,15 @@ import cv2
 import numpy as np
 import matplotlib.pylab as plt
 
+video = cv2.VideoCapture(0)
+video.set(3,640)
 tags = ["Taken", "Empty"]
-parking_lot_files = "parking_lot_empty.jpg"
-image = cv2.imread(parking_lot_files)
+ret, frame = video.read()
 
-cv2.imshow('Image', image)
+#parking_lot_files = "parking_lot_empty.jpg"
+#image = cv2.imread(parking_lot_files)
+
+#cv2.imshow('Image', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
