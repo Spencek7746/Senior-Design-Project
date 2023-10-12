3b678ee95c0fe116bc7a83fde671281e68d8186d
Spencer Karpati <spebkarp@ut.utm.edu>
2023-10-11 19:45:41 -0500

    Fixed logic for VerifyModel, added more xample images

--- Program/VerifyModel.py
+++ Program/VerifyModel.py
@@ -2,17 +2,6 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-#model = YOLO('yolov8n.pt')
-
-def trainModel():
-   results = model.train(
-       data = r'../Data/data.yaml',
-       imgsz = 640,
-       epochs = 250,
-       batch = 8,
-       name = 'test_model'
-       ) 
-
 def verifyModelVideo():
     video_path = r'../examples/example1.mp4'
     video_path_out = '{}_out.mp4'.format(video_path)
@@ -31,7 +20,7 @@ def verifyModelVideo():
         results = model(frame)[0]
 
         for result in results.boxes.data.tolist():
-            x1,y1, x2, y2, score, class_id = result
+            x1, y1, x2, y2, score, class_id = result
 
             if score > threshold:
                 if class_id == 0:  
@@ -47,28 +36,37 @@ def verifyModelVideo():
 def verifyModel():
     model = YOLO(r'../runs/detect/test_model250/weights/best.pt')
 
-    image_path = r'../examples/exampleImg.jpg'
+    image_path = r'../examples/exampleImg4.jpg'
     image = cv2.imread(image_path)
 
-    results = model(image)
+    threshold = 0.5;
+
+    results = model.predict(image, conf=threshold)[0]
+
+    class_name_dict = {0: 'Empty',
+                       1: 'Taken'}
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        x1 = int(x1)
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+        class_id = int(class_id)
+
+        if class_id == 0:  
+            color = (0, 255, 0) 
+        elif class_id == 1: 
+            color = (0, 0, 255) 
+
+        cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, 4)
+        cv2.putText(image, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, color, 3, cv2.LINE_AA)
+
+    cv2.imshow("ParkSense", image)
+    cv2.waitKey(0)
+    cv2.imwrite("../examples/output4.jpg", image)
 
 
-    threshold = 0.5
-
-    for result in results:
-        x1, y1, x2, y2, score, class_id = result[:6]
-
-        if score > threshold:
-                if class_id == 0:  
-                    color = (0, 255, 0) 
-                elif class_id == 1: 
-                    color = (0, 0, 255) 
-
-                cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, 4)
-                cv2.putText(image, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, color, 3, cv2.LINE_AA)
-
-    output_path = '../examples/processed_image.jpg'
-    cv2.imwrite(output_path, image)
-
-#verifyModel()
-verifyModelVideo()
+verifyModel()
+#verifyModelVideo()
